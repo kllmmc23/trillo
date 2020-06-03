cde21ec32fc9c2b5355eee81d50ab999b2c94468
Michael <kllmmc23@gmail.com>
2020-06-03 16:12:57 -0600

    gallery

--- src/App/components/Container/Content/Main.jsx
+++ src/App/components/Container/Content/Main.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
-    return (
-        <main className='hotel-view'>
-            Hotel view
-        </main>
-    )
-}
+  return (
+    <main className="hotel-view">
+      <div className="gallery">
+        <figure className="gallery__item">
+          <img src="img/hotel-1.jpg" alt="hotel 1" className="gallery__photo" />
+        </figure>
+        <figure className="gallery__item">
+          <img src="img/hotel-2.jpg" alt="hotel 2" className="gallery__photo" />
+        </figure>
+        <figure className="gallery__item">
+          <img src="img/hotel-3.jpg" alt="hotel 3" className="gallery__photo" />
+        </figure>
+      </div>
+
+      <div className="overview">
+        <h1 className="overview__heading">Hotel Las Palmas</h1>
+
+        <div className="overview__stars">
+          <svg className="overview__icon-star">
+            <use xlinkHref="img/sprite.svg#icon-star" />
+          </svg>
+          <svg className="overview__icon-star">
+            <use xlinkHref="img/sprite.svg#icon-star" />
+          </svg>
+          <svg className="overview__icon-star">
+            <use xlinkHref="img/sprite.svg#icon-star" />
+          </svg>
+          <svg className="overview__icon-star">
+            <use xlinkHref="img/sprite.svg#icon-star" />
+          </svg>
+          <svg className="overview__icon-star">
+            <use xlinkHref="img/sprite.svg#icon-star" />
+          </svg>
+        </div>
+
+        <div className="overview__location">
+          <svg className="overview__icon-location">
+            <use xlinkHref="img/sprite.svg#icon-location-pin" />
+          </svg>
+          <button className="bt-inline">Albufeira, Portugal</button>
+        </div>
+
+        <div className="overview__rating">
+          <div className="overview__rating-average">8.6</div>
+          <div className="overview__rating-count">429 votes</div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Main;

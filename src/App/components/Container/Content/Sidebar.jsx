3197b134712814a9b7c8813a0d0b1e81e7cb1c48
Michael <kllmmc23@gmail.com>
2020-06-03 11:58:31 -0600

    nav

--- src/App/components/Container/Content/Sidebar.jsx
+++ src/App/components/Container/Content/Sidebar.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div to='/' className='sidebar'>
-            <ul className="side-nav">
-                <li className="side-nav__item">
-                    <NavLink to='/' className='side-nav__link'>
-                        svg
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <nav className="sidebar">
+      <ul className="sidebar-nav">
+        <li className="sidebar-nav__item sidebar-nav__item--active">
+          <NavLink to="/" className="sidebar-nav__link">
+            <svg className="sidebar-nav__icon">
+              <use xlinkHref="img/sprite.svg#icon-home"></use>
+            </svg>
+            <span>Hotel</span>
+          </NavLink>
+        </li>
+
+        <li className="sidebar-nav__item">
+          <NavLink to="/" className="sidebar-nav__link">
+            <svg className="sidebar-nav__icon">
+              <use xlinkHref="img/sprite.svg#icon-aircraft-take-off"></use>
+            </svg>
+            <span>Flight</span>
+          </NavLink>
+        </li>
+
+        <li className="sidebar-nav__item">
+          <NavLink to="/" className="sidebar-nav__link">
+            <svg className="sidebar-nav__icon">
+              <use xlinkHref="img/sprite.svg#icon-key"></use>
+            </svg>
+            <span>Car rental</span>
+          </NavLink>
+        </li>
+
+        <li className="sidebar-nav__item">
+          <NavLink to="/" className="sidebar-nav__link">
+            <svg className="sidebar-nav__icon">
+              <use xlinkHref="img/sprite.svg#icon-map"></use>
+            </svg>
+            <span>Tours</span>
+          </NavLink>
+        </li>
+          </ul>
+          <div className="legal">
+              &copy; 2020 by Trillo. All rights reserved.
+          </div>
+    </nav>
+  );
+};
 
 export default Sidebar;

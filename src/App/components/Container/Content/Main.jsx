980f8076b9810d196b2ec4af02ffe8545a48cca1
Michael <kllmmc23@gmail.com>
2020-06-03 19:57:50 -0600

    recommend

--- src/App/components/Container/Content/Main.jsx
+++ src/App/components/Container/Content/Main.jsx
@@ -48,6 +48,43 @@ const Main = () => {
           <div className="overview__rating-count">429 votes</div>
         </div>
       </div>
+
+      <div className="detail">
+        <div className="description">
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            doloribus, autem saepe soluta voluptatibus qui labore perferendis ab
+            a maiores?
+          </p>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            doloribus, autem saepe soluta voluptatibus qui labore perferendis ab
+            a maiores?
+          </p>
+          <ul className="list">
+            <li className="list__item">Close to the beach</li>
+            <li className="list__item">Breakfast included</li>
+            <li className="list__item">Free airport shuttle</li>
+            <li className="list__item">Free wifi in all rooms</li>
+            <li className="list__item">Air conditioning heating</li>
+            <li className="list__item">Pets allowed</li>
+            <li className="list__item">We speak all languages</li>
+            <li className="list__item">Perfect for families</li>
+          </ul>
+          <div className="recommend">
+            <p className="recommend__count">
+              Lucy amd 3 other friend recommend this hotel
+            </p>
+            <div className="recommend__friends">
+              <img src="img/user-3.jpg" alt="user 3" className="recommend__photo" />
+              <img src="img/user-4.jpg" alt="user 4" className="recommend__photo" />
+              <img src="img/user-5.jpg" alt="user 5" className="recommend__photo" />
+              <img src="img/user-6.jpg" alt="user 6" className="recommend__photo" />
+            </div>
+          </div>
+        </div>
+        <div className="user-reviews">User Reviews</div>
+      </div>
     </main>
   );
 };
